cdc7ff7ad19679a4b435e728501d6bb55abe323c
Joe <jdg006@lvc.edu>
2017-08-31 20:52:17 +0000

    kele checkpoint 6 complete

--- lib/kele.rb
+++ lib/kele.rb
@@ -26,6 +26,52 @@ include HTTParty
        JSON.parse(response.body)
   end
   
+  def get_messages(page_number = 0)
+      
+      if page_number == 0
+        
+        all_pages = []
+        response = self.class.get("https://www.bloc.io/api/v1/message_threads", headers: { "authorization" => @auth_token })
+        message_count = JSON.parse(response.body)["count"]
+        page_count = message_count/10
+        
+        if message_count % 10 > 0
+           page_count += 1
+        end
+        
+        i = 1
+        while i <= page_count do
+          all_pages.push(JSON.parse(self.class.get("https://www.bloc.io/api/v1/message_threads", body: {"page": i,}, headers: { "authorization" => @auth_token }).body))
+          i += 1
+        end
+        return all_pages
+        
+      else
+        response = self.class.get("https://www.bloc.io/api/v1/message_threads", body: {"page": page_number,}, headers: { "authorization" => @auth_token })
+          return JSON.parse(response.body)
+      end
+      
+  end
+  
+  def create_message(message, recipient_id, subject, thread_token = nil)
+    if thread_token == nil
+      self.class.post("https://www.bloc.io/api/v1/messages", body: {
+      "sender": "jdg006@lvc.edu",
+      "recipient_id": recipient_id,
+      "subject": subject,
+      "stripped-text": message},
+      headers: { "authorization" => @auth_token })
+    else
+      self.class.post("https://www.bloc.io/api/v1/messages", body: {
+      "sender": "jdg006@lvc.edu",
+      "recipient_id": recipient_id,
+      "token": thread_token,
+      "subject": subject,
+      "stripped-text": message},
+      headers: { "authorization" => @auth_token })
+    end
+  end
+  
   
 end
 
